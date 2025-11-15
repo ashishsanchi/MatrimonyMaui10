258bcd379b4c902accf902a8ae354664ed8f8eaf
ashishsanchi <65947632+ashishsanchi@users.noreply.github.com>
2025-11-15 22:05:13 +0530

    fix: adopt CreateWindow initialization

---
+++ MatrimonyApp-MAUI/Matrimony.Mobile/MauiProgram.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Networking;
+
+namespace Matrimony.Mobile;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>();
+
+        builder.Services.AddSingleton<IConnectivity>(_ => Connectivity.Current);
+        builder.Services.AddSingleton<AppShell>();
+        builder.Services.AddSingleton<Views.HomePage>();
+        builder.Services.AddSingleton<Views.ProfilePage>();
+        builder.Services.AddSingleton<ViewModels.HomeViewModel>();
+        builder.Services.AddSingleton<ViewModels.ProfileViewModel>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
